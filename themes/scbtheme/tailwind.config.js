5dadcbc5b8e5ac653e514ee0c2be81ca3a68caa9
Steven C. Buttgereit <github.com@musesystems.com>
2023-05-22 16:03:06 -0700

    Update font family to something more basic
    This will serve as a test to see if we're getting a rebuild of style.css
    on deploy at Netlify... I expect not (not sure it would know how to do
    that).

--- themes/scbtheme/tailwind.config.js
+++ themes/scbtheme/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["content/**/*.md", "content/**/*.html", "../../content/**/*.md", "../../content/**/*.html", "layouts/**/*.html", "../../layouts/**/*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif']
+    }
   },
   plugins: [],
 }
